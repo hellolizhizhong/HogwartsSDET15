ea88cfa9acbee4a8e69540f70bd5833302838a06
Administrator <hellolizhizhong@163.com>
2020-10-18 23:06:20 +0800

    pytest 实战 一

--- calculator.py
+++ calculator.py
@@ -7,27 +7,25 @@ from numbers import Number
 
 class Calculator:
 	def add(self, a, b):
-		if isinstance(a,Number) | isinstance(b,Number):
+		if isinstance(a, Number) | isinstance(b, Number):
 			return a + b;
 		else:
 			print("不支持非数字的输入")
 
 	def sub(self, a, b):
-		if isinstance(a,Number) | isinstance(b,Number):
+		if isinstance(a, Number) | isinstance(b, Number):
 			return a - b;
 		else:
 			print("不支持非数字的输入")
 
 	def mul(self, a, b):
-		if isinstance(a,Number) | isinstance(b,Number):
+		if isinstance(a, Number) | isinstance(b, Number):
 			return a * b;
 		else:
 			print("不支持非数字的输入")
 
-
 	def div(self, a, b):
-		if isinstance(a,Number) | isinstance(b,Number):
-			return a /b;
+		if isinstance(a, Number) | isinstance(b, Number):
+			return a / b;
 		else:
 			print("不支持非数字的输入")
-
